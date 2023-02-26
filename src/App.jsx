74eb07f1ad1f380d1cc868afc20bed4bd53d4742
Josselyn Polanco <vanesitapolanco@gmail.com>
2023-02-25 22:02:52 -0600

    [ADDED]Add information in localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react'
+import Formulario from "./components/Formulario"
 import Header from "./components/Header"
-import Formulario from "./components/Formulario";
-import ListadoPacientes from "./components/ListadoPacientes";
+import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
 
-  const [pacientes, setPacientes] = useState([]);
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? []);
   const [paciente, setPaciente] = useState({});
+
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify( pacientes ));
+  }, [pacientes])
 
   const eliminarPaciente = (id) =>{
     const pacientesActualizados = pacientes.filter(itemPaciente => itemPaciente.id != id);
